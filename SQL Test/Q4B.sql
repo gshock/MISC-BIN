c15e37abca5d197bdccb70c09817965be8249c8e
Gerardo A <zdoggie@gmail.com>
2015-12-02 14:38:57 -0600

    some code i wrote for a sql test

---
+++ SQL Test/Q4B.sql
@@ -0,0 +1,15 @@
+USE [Deloitte]
+GO
+
+DECLARE @portf_id int
+DECLARE @as_of_date datetime 
+
+SET @portf_id = 1 
+SET @as_of_date = '1/1/12'
+
+SELECT TF1.issue_id, SUM(TF1.outstanding_amount) AS aggregate_amt, AVG (
+		TF1.outstanding_amount / a.purchase_amount) AS weighted_avg_purch_price
+	FROM [dbo].[tf_Asset_Portfolio] (@portf_id,@as_of_date) AS TF1
+	INNER JOIN asset a ON a.asset_id = TF1.asset_id 
+	GROUP BY TF1.issue_id
+GO
